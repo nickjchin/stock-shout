57679b96b9979ad9438a59cd1c9028f53dcdcbf6
Michael Rohmann <mrohmann@gmail.com>
2021-09-15 13:31:23 -0400

    updated home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,6 +7,31 @@ router.get('/', async (req, res) => {
     res.render('layouts/main');
 });
 
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
