3c3461e274e8230abe6084e175639952b3daf34c
Michael Rohmann <mrohmann@gmail.com>
2021-09-19 19:47:22 -0400

    added NASDAQ listings to seed file

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,3 @@
-const stockSearchFormHandler = async (event) => {
-  event.preventDefault();
-
-  const stock = document.querySelector("#entersymbol").value.trim();
-
-//   if (stock) {
-//     const response = await fetch("", {
-//       method: "POST",
-//       body: JSON.stringify({ stock }),
-//       headers: { "Content-Type": "application/json" },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace("results");
-//     } else {
-//       alert("No Stock Symbol Found!");
-//     }
-//   }
-};
 
 const loginHandler = async (event) => {
   event.preventDefault();
