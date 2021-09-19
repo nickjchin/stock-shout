db31f3f4a45ef6efbf0bd0e3489a00357595d6e1
Michael Rohmann <mrohmann@gmail.com>
2021-09-19 13:44:17 -0400

    added profile display handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, UserStock} = require('../models');
 const withAuth = require('../utils/auth');
 
-var Twitter = require('twitter');
+// var Twitter = require('twitter');
 
 router.get('/', async (req, res) => {
  
@@ -31,29 +31,29 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
-router.get('/searchresults', withAuth, async (req, res) => {
-  console.log("results page");
-  //console.log("stock id",req.body);
-  var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    bearer_token: process.env.TWITTER_BEARER_TOKEN,
-  });
+// router.get('/searchresults', withAuth, async (req, res) => {
+//   console.log("results page");
+//   //console.log("stock id",req.body);
+//   var client = new Twitter({
+//     consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//     bearer_token: process.env.TWITTER_BEARER_TOKEN,
+//   });
 
-   try {
-    // 
-    const tweetData = await client.get("2/tweets/counts/recent", { q: "gme" }, function (error, tweets, response){ });
+//    try {
+//     // 
+//     const tweetData = await client.get("2/tweets/counts/recent", { q: "gme" }, function (error, tweets, response){ });
 
-    const tweetCount = tweetData.get({ plain: true });
-    console.log(tweetCount);
-    res.render('searchresult', {
-      ...tweetCount,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     const tweetCount = tweetData.get({ plain: true });
+//     console.log(tweetCount);
+//     res.render('searchresult', {
+//       ...tweetCount,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
