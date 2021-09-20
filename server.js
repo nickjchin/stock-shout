c28a454d665b7076257acb69c477de6ad41c79e3
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-09-20 09:31:27 -0400

    saving for pull

--- server.js
+++ server.js
@@ -1,40 +1,162 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const http = require("http");
+const path = require("path");
+const needle = require("needle");
+const socketIo = require("socket.io");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const Token = process.env.TWITTER_BEARER_TOKEN;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize,
-    }),
-  };
-  
-  app.use(session(sess));
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
+app.use(session(sess));
 
-const hbs = exphbs.create({helpers});
+const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log("Now listening"));
+// });
+
+// Twitter Stream
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../", "views/layouts", "searchresults.handlebars"));
+});
+
+const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
+const streamURL =
+  "https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id";
+
+const rules = [{ value: "amc" }];
+
+// Get stream rules
+async function getRules() {
+  const response = await needle("get", rulesURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+  console.log(response.body);
+  return response.body;
+}
+
+// Set stream rules
+async function setRules() {
+  const data = {
+    add: rules,
+  };
+
+  const response = await needle("post", rulesURL, data, {
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  return response.body;
+}
+
+// Delete stream rules
+async function deleteRules(rules) {
+  if (!Array.isArray(rules.data)) {
+    return null;
+  }
+
+  const ids = rules.data.map((rule) => rule.id);
+
+  const data = {
+    delete: {
+      ids: ids,
+    },
+  };
+
+  const response = await needle("post", rulesURL, data, {
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  return response.body;
+}
+
+function streamTweets(socket) {
+  const stream = needle.get(streamURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  stream.on("data", (data) => {
+    try {
+      const json = JSON.parse(data);
+      console.log(json);
+      socket.emit("tweet", json);
+    } catch (error) {}
+  });
+
+  return stream;
+}
+
+io.on("connection", async () => {
+  console.log("Client connected...");
+
+  let currentRules;
+
+  try {
+    //   Get all stream rules
+    currentRules = await getRules();
+
+    // Delete all stream rules
+    await deleteRules(currentRules);
+
+    // Set rules based on array above
+    await setRules();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  const filteredStream = streamTweets(io);
+
+  let timeout = 0;
+  filteredStream.on("timeout", () => {
+    // Reconnect on error
+    console.warn("A connection error occurred. Reconnecting…");
+    setTimeout(() => {
+      timeout++;
+      streamTweets(io);
+    }, 2 ** timeout);
+    streamTweets(io);
+  });
+});
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
